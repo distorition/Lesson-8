9643c0d46964128e899589eb087bd0360682703a
distorition <85959781+distorition@users.noreply.github.com>
2021-07-12 16:12:31 +0300

    Add files via upload

---
+++ Lesson 8/ExampleLibriry/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ExampleLibriry
+{
+   public  class Greeting
+    {
+        public static void SayHello()
+        {
+            System.Console.WriteLine("Hello from ExampleLibrary!");
+        }
+
+    }
+}
